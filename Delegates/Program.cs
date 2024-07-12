3773600a42758e6713f9ba3dc5f9080d1782a11f
Yusif70 <yusifpirquliyev7@gmail.com>
2024-07-12 10:08:30 +0400

    initial commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using Delegates.task1;
+using Delegates.task2;
+using Delegates.task3;
+
+#region task1
+
+Func<int, int> square = Class1.Square;
+Console.WriteLine(square(5));
+
+#endregion
+
+#region task2
+
+Func<double, double> convert = Class2.ConvertTemp;
+Console.WriteLine(convert(50));
+
+#endregion
+
+#region task3
+
+string sourcePath = "C:\\Users\\Lenovo\\source\\repos\\Delegates\\Delegates\\task3\\TextFile1.txt";
+string destpath = "C:\\Users\\Lenovo\\source\\repos\\Delegates\\Delegates\\task3\\TextFile2.bin";
+
+Action<string> readFIle = Class3.ReadFile;
+readFIle(sourcePath);
+
+Action<string, string> writeFile = Class3.WriteFile;
+writeFile(sourcePath, "a, b, c");
+
+Action<string, string> encrypt = Class3.Encrypt;
+encrypt(sourcePath, destpath);	
+
+#endregion
---
+++ Delegates/task3/Class3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.task3
+{
+	public class Class3
+	{
+		public delegate void Action();
+		public static void ReadFile(string path)
+		{
+			string[] lines = File.ReadAllLines(path);
+			foreach (string line in lines)
+			{
+                Console.WriteLine(line);
+            }
+		}
+		public static void WriteFile(string path, string content)
+		{
+			File.AppendAllText(path, content);
+		}
+		public static void Encrypt(string sourcePath, string destPath)
+		{
+			string[] lines = File.ReadAllLines(sourcePath);
+			Stream st = File.Open(destPath, FileMode.OpenOrCreate);
+			BinaryFormatter bf = new();
+			bf.Serialize(st, lines);
+			st.Close();
+		}
+	}
+}
